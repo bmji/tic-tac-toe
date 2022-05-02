85ff4dfc63677310ff596509d6a9440846f505be
bmji <alicia.bang404@gmail.com>
2022-05-02 19:11:17 +1000

    project 1

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Tic Tac Toe
 ## Summary of project
+A simple game of Tic-Tac Toe! 
+
+This project allows the user to go through various player icons and participate in a 2 player game of Tic-Tac-Toe! A restart button is also available in the case of the game coming to an end! 
+
+Win scenarios and a draw scenario has been included to replicate the original game. 
+
 ## Plan / approach you took to solving the problem
 ![tictactoe](./images/Tic%20Tac%20Toe%20plan.png)
 ![baseplan](./images/Screen%20Shot%202022-05-02%20at%202.39.12%20am.png)
+
 ## Cool tech you used (or code you're proud of)
+I have implemented the use of a bootstrap carousel to allow the user to choose an icon of their choice. It was quite difficult to code as many issues popped up along the way, though in the end, I was able to find solutions from thorough research as well as trial and error.
+
+I am also very proud of the win + draw scenarios since it was pretty much the main part of the project. Being able to think of the logic behind everything and applying it into code was something that I was personally able to feel very proud of.
+
 ## Lessons you learnt
+From this project, I have learnt that not everything will click into place immediately. Coding takes time and patience. It will drain a lot of your mental energy in one sitting, and I learnt that when that happens, it is best to take a short break and come back to the code for a more refreshed mindset. 
+
+Bonus lesson: Every day before the due-date is a do-date.
 
 <a href="https://bmji.github.io/tic-tac-toe/">Click here to play the game</a>
--- main.js
+++ main.js
@@ -1,38 +1,61 @@
-win_conditions = [
-    {b1, b2, b3},
-    {b4, b5, b6},
-    {b7, b8, b9},
-    {b1, b4, b7},
-    {b2, b5, b8},
-    {b3, b6, b9},
-    {b1, b5, b9},
-    {b3, b5, b8}
-]
-
-//how to check if player has won
-    for (var i = 0; i < allBoxes.length; i++) {
-        if (allBoxes.length[i] == 3) {
-            textToAdd.value = 'X'
-        }
-    }
-
 //switching between players
 var currentTurn = 'X'
 var nextTurn = 'O'
 var isOTurn = false
 
-while (isOTurn != true) {
-    allBoxes.addEventListener('click', function(event) {
-        if (event.target.tagName === 'DIV') {
-            textToAdd.value = 'currentTurn'
-            event.target.textContent = textToAdd.value
-            let isOTurn = true
-        } else if (isOTurn == true) {
-            if (event.target.tagName === 'DIV') {
-                textToAdd.value = 'nextTurn'
-                event.target.textContent = textToAdd.value
-                let isOTurn = false
-            }
-        }
-    })
+var allBoxes = document.querySelector('.boxes')
+var gameResult = document.querySelector('.game-result')
+var box1 = document.querySelector('#b1')
+var box2 = document.querySelector('#b2')
+var box3 = document.querySelector('#b3')
+var box4 = document.querySelector('#b4')
+var box5 = document.querySelector('#b5')
+var box6 = document.querySelector('#b6')
+var box7 = document.querySelector('#b7')
+var box8 = document.querySelector('#b8')
+var box9 = document.querySelector('#b9')
+var restartGame = document.querySelector('.restart')
+
+
+//how to check if player has won
+function checkWin(player) {
+    if (box1.textContent == player && box2.textContent == player && box3.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box4.textContent == player && box5.textContent == player && box6.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box7.textContent == player && box8.textContent == player && box9.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box1.textContent == player && box4.textContent == player && box7.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box2.textContent == player && box5.textContent == player && box8.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box3.textContent == player && box6.textContent == player && box9.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box1.textContent == player && box5.textContent == player && box9.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box3.textContent == player && box5.textContent == player && box7.textContent == player) {
+        gameResult.textContent = player + ' has won!'
+    } else if (box1.textContent != '' && box2.textContent != '' && box3.textContent != '' && box4.textContent != '' && box5.textContent != '' && box6.textContent != '' && box7.textContent != '' && box8.textContent != '' && box9.textContent != '') {
+        gameResult.textContent = 'Draw!'
+    }
 }
+
+allBoxes.addEventListener('click', function(event) {
+    if (event.target.textContent != '') {
+        alert('This box is already checked!')
+    } else if (isOTurn != true) {
+        event.target.textContent = currentTurn
+        event.target.classList.add('design')
+        checkWin(event.target.textContent)
+        isOTurn = !isOTurn
+    } else if (isOTurn == true) {
+        event.target.textContent = nextTurn
+        event.target.classList.add('design')
+        checkWin(event.target.textContent)
+        isOTurn = !isOTurn
+    }
+})
+
+restartGame.addEventListener('click', function(event) {
+    location.reload();
+})
